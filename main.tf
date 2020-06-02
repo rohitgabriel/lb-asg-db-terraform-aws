aa63035a45e96015e29825a6cd1b65813d877f00
Rohit Gabriel <rohit.gabriel@enterpriseit.co.nz>
2020-06-02 14:34:28 +1200

    check vars3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  version    = ">= 2.53"
-  region     = var.AWS_REGION
+  version = ">= 2.53"
+  region  = var.AWS_REGION
   # access_key = var.AWS_ACCESS_KEY_ID
   # secret_key = var.AWS_SECRET_ACCESS_KEY
 }
