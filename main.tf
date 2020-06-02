ed62262f47677866e28e474716b42fa49dbad38a
Rohit Gabriel <rohit.gabriel@enterpriseit.co.nz>
2020-06-02 14:46:45 +1200

    updated with -var

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  version = ">= 2.53"
-  region  = var.AWS_REGION
-  # access_key = var.AWS_ACCESS_KEY_ID
-  # secret_key = var.AWS_SECRET_ACCESS_KEY
+  version    = ">= 2.53"
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 terraform {
